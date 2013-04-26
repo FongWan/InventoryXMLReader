3604ae942945ffabf38f71e807ce8496adbe9859
Fong-Wan Chau <yo@fong-wan.me>
2013-04-26 18:02:17 -0400

    Display more data
    + Added more code to display more data
    + Fixed bug that display only the first memory capacity
    
    Signed-off-by: Fong-Wan Chau <yo@fong-wan.me>

--- reader.php
+++ reader.php
@@ -53,7 +53,7 @@ if (isset($_GET['fileName']) && !empty($_GET['fileName'])) {
       echo '<tr><th>Procesadores</th><td>';
 
       // Revisar si hay más de un procesaodr
-      if (is_array($fileData->Processors->Processor)) {
+      if (1 < sizeof($fileData->Processors->Processor)) {
         echo '<ol>';
 
         // Revisar todas las tarjetas gráficas
@@ -89,7 +89,7 @@ if (isset($_GET['fileName']) && !empty($_GET['fileName'])) {
       $currentUnit = 0;
 
       // Revisar si hay más de una memoria RAM
-      if (is_array($fileData->PhysicalMemory->Memory)) {
+      if (1 < sizeof($fileData->PhysicalMemory->Memory)) {
         // Sumar capacidades de memoria RAM
         for ($i = 0, $totalSlot = sizeof($fileData->PhysicalMemory->Memory); $i < $totalSlot; ++$i) {
           $totalMemory += $fileData->PhysicalMemory->Memory[$i]->Capacity;
@@ -124,7 +124,7 @@ if (isset($_GET['fileName']) && !empty($_GET['fileName'])) {
       echo '<tr><th>Tarjetas gráficos</th><td>';
 
       // Revisar si hay más de una tarjeta gráficos
-      if (is_array($fileData->VideoCards->Card)) {
+      if (1 < sizeof($fileData->VideoCards->Card)) {
         echo '<ol>';
 
         // Revisar todas las tarjetas gráficas
@@ -142,9 +142,111 @@ if (isset($_GET['fileName']) && !empty($_GET['fileName'])) {
       echo '</td></tr>';
     }
 
+    // Si existe datos de los adaptadores de red
+    if (isset($fileData->NetworkAdapters)) {
+      // Imprimir datos de las tarjetas gráficos
+      echo '<tr><th>Adaptadores de red</th><td>';
+
+      // Revisar si hay más de una tarjeta gráficos
+      if (is_object($fileData->NetworkAdapters->Adapter)) {
+        echo '<ol>';
+
+        // Revisar todas las tarjetas gráficas
+        for ($i = 0, $totalNetworkAdapters = sizeof($fileData->NetworkAdapters->Adapter); $i < $totalNetworkAdapters; ++$i) {
+          // Revisar si son tarjetas reales buscando la MAC
+          if (!empty($fileData->NetworkAdapters->Adapter[$i]->MACAddress)) {
+            // Imprimir nombre de cada tarjeta
+            echo '<li>' . $fileData->NetworkAdapters->Adapter[$i]->Description . '</li>';
+          }
+        }
+
+        echo '</ol>';
+      } else {
+        // Imprimir nombre de la tarjeta
+        echo $fileData->NetworkAdapters->Adapter->Description;
+      }
+
+      echo '</td></tr>';
+    }
+
+    // Si existe datos de las tarjetas de sonidos
+    if (isset($fileData->SoundDevice)) {
+      // Imprimir datos de las tarjetas de sonidos
+      echo '<tr><th>Tarjetas de sonidos</th><td>';
+
+      // Revisar si hay más de una tarjeta de sonidos
+      if (1 < sizeof($fileData->SoundDevice->Device)) {
+        echo '<ol>';
+
+        // Revisar todas las tarjetas de sonidos
+        for ($i = 0, $totalSoundDevice = sizeof($fileData->SoundDevice->Device); $i < $totalSoundDevice; ++$i) {
+          // Imprimir nombre de cada tarjeta
+          echo '<li>' . $fileData->SoundDevice->Device[$i]->Name . '</li>';
+        }
+
+        echo '</ol>';
+      } else {
+        // Imprimir nombre de la tarjeta
+        echo $fileData->SoundDevice->Device->Name;
+      }
+
+      echo '</td></tr>';
+    }
+
+
+    // Si existe datos de las impresoras
+    if (isset($fileData->Printers)) {
+      // Imprimir datos de las impresoras
+      echo '<tr><th>Impresoras</th><td>';
+
+      // Revisar si hay más de una impresora
+      if (1 < sizeof($fileData->Printers->Printer)) {
+        echo '<ol>';
+
+        // Revisar todas las tarjetas gráficas
+        for ($i = 0, $totalSoundDevice = sizeof($fileData->Printers->Printer); $i < $totalSoundDevice; ++$i) {
+          // Imprimir nombre de cada tarjeta
+          echo '<li>' . $fileData->Printers->Printer[$i]->Name . '</li>';
+        }
+
+        echo '</ol>';
+      } else {
+        // Imprimir nombre de la tarjeta
+        echo $fileData->Printers->Printer->Name;
+      }
+
+      echo '</td></tr>';
+    }
+
+    // Si existe datos del sistema operativo
     if (isset($fileData->OperatingSystem)) {
+      // Imprimir el tipo de sistema operativo
       echo '<tr><th>Sistema operativo</th><td>' . $fileData->OperatingSystem->ProductName . '</td></tr>';
       echo '<tr><th>Nombre del equipo</th><td>' . $fileData->OperatingSystem->ComputerName . '</td></tr>';
+    }
+
+    // Si existe datos de los programas instalados
+    if (isset($fileData->Software)) {
+      // Imprimir datos de las tarjetas de sonidos
+      echo '<tr><th>Softwares</th><td>';
+
+      // Revisar si hay más de una tarjeta gráficos
+      if (1 < sizeof($fileData->Software->Product)) {
+        echo '<ol>';
+
+        // Revisar todas las tarjetas gráficas
+        for ($i = 0, $totalSoundDevice = sizeof($fileData->Software->Product); $i < $totalSoundDevice; ++$i) {
+          // Imprimir nombre de cada tarjeta
+          echo '<li>' . $fileData->Software->Product[$i]->Name . '</li>';
+        }
+
+        echo '</ol>';
+      } else {
+        // Imprimir nombre de la tarjeta
+        echo $fileData->Software->Product->Name;
+      }
+
+      echo '</td></tr>';
     }
 
     echo '</table>';
